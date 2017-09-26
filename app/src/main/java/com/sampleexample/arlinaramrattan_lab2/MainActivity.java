898965907d916ec1a8efaa9f8cc31a80147e52d6
aramrat1 <300799246@cencol.ca>
2017-09-26 14:42:08 -0400

    other folders

---
+++ app/src/main/java/com/sampleexample/arlinaramrattan_lab2/MainActivity.java
@@ -0,0 +1,17 @@
+package com.sampleexample.arlinaramrattan_lab2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void goToMenu(){
+        //needs to go to the second activity with the list of food types
+    }
+}
